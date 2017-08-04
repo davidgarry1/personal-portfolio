fa5915c3a62a89c1c29ef5a603f1df6b7add1034
David Garry <dgarry@usc.edu>
2017-08-04 15:38:55 -0700

    Add files via upload

---
+++ Spin/main.js
@@ -0,0 +1,20 @@
+var div = document.getElementById('fadetitle'),
+    letters = div.textContent.split('');
+
+while(div.hasChildNodes()) div.removeChild(div.firstChild);
+
+for(var i = 0; i < letters.length; i++) {
+  var letter = document.createElement('span'),
+      style = 'opacity ' + (Math.random() * 1) + 's linear';
+  letter.appendChild(document.createTextNode(letters[i]));
+    letter.style.WebKitTransition = letter.style.MozTransition = 
+    letter.style.MSTransition = letter.style.OTransition = letter.style.transition = style;
+  letter.style.opacity = 0;
+  div.appendChild(letter);
+}
+
+setTimeout(function() {
+  for(var i = 0; i < div.childNodes.length; i++) {
+    div.childNodes[i].style.opacity = 1;
+  }
+}, 0);
