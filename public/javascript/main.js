dae15ce1a70cb2b91f40c3b683f67e7e7e6a2bfb
David Garry <dgarry@usc.edu>
2017-08-06 22:12:51 -0700

    me

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,3 +1,14 @@
+// Parse the URL parameter
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 var div = document.getElementById('fadetitle'),
     letters = div.textContent.split('');
 
@@ -19,26 +30,34 @@ setTimeout(function() {
   }
 }, 0);
 
+if(getParameterByName("c")){
+  $("#intro").hide();
+  $("#"+c).show();
+  if(c=="thanks"){
+    $("#fadetitle").css("background-image","url(../glow.jpg)");
+  }
+}
+
 $("document").ready(function(){
   history.pushState({urlPath:'/'},"",'/');
   $("#introtext").click(function(){
       swap("#intro","#about","");
-      history.pushState({urlPath:'/'},"",'#about');
+      history.pushState({urlPath:'/'},"",'?c=about');
   });
   $("#abouttext").click(function(){
       swap("#about","#experience","");
-      history.pushState({urlPath:'/'},"",'#experience');
+      history.pushState({urlPath:'/'},"",'?c=experience');
       //Preload image
       img1 = new Image();
       img1.src = "../glow.jpg";
   });
   $("#experiencetext").click(function(){
       swap("#experience","#projects","");
-      history.pushState({urlPath:'/'},"",'#projects');
+      history.pushState({urlPath:'/'},"",'?c=projects');
   });
   $("#projectstext").click(function(){
       swap("#projects","#resume","../glow.jpg");
-      history.pushState({urlPath:'/'},"",'#thanks');
+      history.pushState({urlPath:'/'},"",'?c=thanks');
   });
   $("#resumetext").click(function(){
       swap("#resume","#intro","");
