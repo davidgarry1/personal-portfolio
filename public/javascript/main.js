20199fc1ed927982e136b7d7e3c00883ab02e91c
David Garry <dgarry@usc.edu>
2017-08-08 20:36:32 -0700

    Update main.js

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,3 +1,29 @@
+setInterval(function(){
+  animateLine();
+},1400);
+
+function animateLine(){
+  
+  var path = document.querySelector('.squiggle-animated path');
+var length = path.getTotalLength();
+// Clear any previous transition
+path.style.transition = path.style.WebkitTransition =
+  'none';
+// Set up the starting positions
+path.style.strokeDasharray = length + ' ' + length;
+path.style.strokeDashoffset = length;
+// Trigger a layout so styles are calculated & the browser
+// picks up the starting position before animating
+path.getBoundingClientRect();
+// Define our transition
+path.style.transition = path.style.WebkitTransition =
+  'stroke-dashoffset 1s ease-in-out';
+// Go!
+path.style.strokeDashoffset = '0';
+ 
+}
+
+
 history.pushState({urlPath:'/'},"",'/');
 
 var div = document.getElementById('fadetitle'),
