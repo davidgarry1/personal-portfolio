176b236b6a50b414cd119107decc657e9eb22eaa
David Garry <dgarry@usc.edu>
2017-08-20 18:34:07 -0700

    Update main.js

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -70,6 +70,8 @@ var version = detectIE();
 if(version != false) {
   $(".shape").addClass("altbrowser");
   $(".text").addClass("altbrowser");
+  $("path").addClass("animated");
+  $("path").addClass("fadeIn");
 }
 
 $("document").ready(function(){
